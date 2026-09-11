030b91bedb239532a647169685d511846c9d50ad
Asmitta <tiwabrayan@gmail.com>
2026-09-11 12:41:09 +0200

    refactor: Improve HTML structure and readability in twig templates

--- src/Controller/SalesBoardController.php
+++ src/Controller/SalesBoardController.php
@@ -17,37 +17,49 @@ class SalesBoardController extends AbstractController
     #[Route('/dashboard', name: 'dashboard')]
     public function dashboard(DashboardService $dashboardService): Response
     {
-        return $this->render('dashboard/index.html.twig', ['stats' => $dashboardService->getStatistics()]);
+        return $this->render('dashboard/index.html.twig', [
+            'stats' => $dashboardService->getStatistics(),
+        ]);
     }
 
     #[Route('/sales', name: 'sales')]
     public function sales(EntityManagerInterface $em): Response
     {
-        return $this->render('sales/index.html.twig', ['sales' => $em->getRepository(Sale::class)->findBy([], ['saleDate' => 'DESC'])]);
+        return $this->render('sales/index.html.twig', [
+            'sales' => $em->getRepository(Sale::class)->findBy([], ['saleDate' => 'DESC']),
+        ]);
     }
 
     #[Route('/customers', name: 'customers')]
     public function customers(EntityManagerInterface $em): Response
     {
-        return $this->render('customers/index.html.twig', ['customers' => $em->getRepository(Customer::class)->findBy([], ['lastName' => 'ASC'])]);
+        return $this->render('customers/index.html.twig', [
+            'customers' => $em->getRepository(Customer::class)->findBy([], ['lastName' => 'ASC']),
+        ]);
     }
 
     #[Route('/products', name: 'products')]
     public function products(EntityManagerInterface $em): Response
     {
-        return $this->render('products/index.html.twig', ['products' => $em->getRepository(Product::class)->findBy([], ['name' => 'ASC'])]);
+        return $this->render('products/index.html.twig', [
+            'products' => $em->getRepository(Product::class)->findBy([], ['name' => 'ASC']),
+        ]);
     }
 
     #[Route('/transactions', name: 'transactions')]
     public function transactions(EntityManagerInterface $em): Response
     {
-        return $this->render('transactions/index.html.twig', ['transactions' => $em->getRepository(Transaction::class)->findBy([], ['transactionDate' => 'DESC'])]);
+        return $this->render('transactions/index.html.twig', [
+            'transactions' => $em->getRepository(Transaction::class)->findBy([], ['transactionDate' => 'DESC']),
+        ]);
     }
 
     #[Route('/reports', name: 'reports')]
     public function reports(DashboardService $dashboardService): Response
     {
-        return $this->render('reports/index.html.twig', ['stats' => $dashboardService->getStatistics()]);
+        return $this->render('reports/index.html.twig', [
+            'stats' => $dashboardService->getStatistics(),
+        ]);
     }
 
     #[Route('/settings', name: 'settings')]
